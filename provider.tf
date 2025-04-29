a53edfeb66106dbfc8fafce66b465b06852bdbb1
A.Mohan <mohan6121@email.com>
2025-04-29 07:54:22 +0530

    pracctice

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-credentials = file("terraform.json")
+credentials = file("C:\Users\91912\OneDrive\Desktop\AI projects\DEVOPS\Terraform-GCP1\terraform.json")
   project     = var.project_id
   region      = var.region
   zone        = var.zone
